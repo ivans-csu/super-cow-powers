3d7eb143e3deb0ace0e81ad3a32f441f3f441ae5
Benjamin Perry <babylfish@babyl.xyz>
2024-10-31 11:59:15 -0600

    boardstate dataclass, spec clarification

--- shared.py
+++ shared.py
@@ -62,3 +62,55 @@ class PushPreamble:
 
     def pack(self) -> bytes:
         return struct.pack('!H', self.type)
+
+class SQUARE(IntEnum):
+    EMPTY = 0
+    BLACK = 1
+    WHITE = 2
+
+class BoardState:
+    _MASK = 0x3
+    _MAP = b'-@O'
+
+    def __init__(self, new=True):
+        self.state: list[list[SQUARE]] = [[SQUARE.EMPTY] * 8 for _ in range(8)]
+        if new:
+            self.state[3][3] = self.state[4][4] = SQUARE.WHITE
+            self.state[3][4] = self.state[4][3] = SQUARE.BLACK
+
+    def __repr__(self):
+        output = bytearray(128)
+        i = 0
+        for row in range(8):
+            for col in range(8):
+                output[i] = BoardState._MAP[self.state[row][col]]
+                output[i+1] = 0x20 # ' '
+                i += 2
+            output[i-1] = 0x0A # '\n'
+        return output.decode('ascii')
+
+    @staticmethod
+    def unpack(message: bytes) -> 'BoardState':
+        board = BoardState(new=False)
+        octet = 0
+        for row in range(8):
+            for offset in (0,4):
+                pack = message[octet]
+                for col in range(3,-1,-1):
+                    board.state[row][col + offset] = SQUARE(pack & BoardState._MASK)
+                    pack >>= 2
+                octet += 1
+        return board
+
+    def pack(self) -> bytes:
+        output = bytearray(16)
+        octet = 0
+        for row in range(8):
+            for offset in (0,4):
+                pack = 0
+                for col in range(4):
+                    pack <<= 2
+                    pack |= self.state[row][col + offset]
+                output[octet] = pack
+                octet += 1
+        return bytes(output)
